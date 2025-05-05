14e0e0518b0a401d1ea9cdf107f0123b8fe52306
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2025-05-06 00:19:44 +0700

    update admin web-page

---
+++ admin/src/routes/paths.jsx
@@ -0,0 +1,10 @@
+export const rootPaths = {
+  root: '/Fashion_Baki_Admin/',
+  pagesRoot: 'pages',
+  authRoot: '/Fashion_Baki_Admin/authentication',
+};
+
+export default {
+  signin: `${rootPaths.authRoot}/signIn`,
+  signup: `${rootPaths.authRoot}/signUp`,
+};
